181c55aab64db74ea1beb043024076134c58f1dd
Wen Kokke <wenkokke@users.noreply.github.com>
2016-05-17 21:07:56 +0200

    Added switch to switch between GRU and RNN

--- code/main.py
+++ code/main.py
@@ -1,9 +1,11 @@
-import sys
 import theano
 import theano.tensor as T
 import numpy         as np
+import sys
 import read_data
 
+
+from rnn     import RNN
 from gru     import GRU
 from encoder import Encoder
 from decoder import Decoder
@@ -12,20 +14,20 @@ from decoder import Decoder
 EMBEDDING_SIZE = 500
 DECODER_SIZE   = 1000
 ENCODER_SIZE   = 1000
-NN_CLASS       = GRU
+NN_CLASS_DICT  = { "RNN": RNN, "GRU": GRU }
 
 
 class Seq2Seq_RNN:
-    def __init__(self, K):
+    def __init__(self, NNClass, K):
 
-        encoder_nnet = NN_CLASS(
+        encoder_nnet = NNClass(
             K, EMBEDDING_SIZE, ENCODER_SIZE,
             use_context_vector=False, E=None)
         self.encoder = Encoder(
             encoder_nnet,
             K, EMBEDDING_SIZE, ENCODER_SIZE)
 
-        decoder_nnet = NN_CLASS(
+        decoder_nnet = NNClass(
             K, EMBEDDING_SIZE, DECODER_SIZE,
             use_context_vector=True, E=encoder_nnet.E)
         self.decoder = Decoder(
@@ -72,45 +74,52 @@ def decode_sentence(sentence, encoding):
 def main():
     """
     Expects a path to the training data as the first argument, for example:
-    python2 code/rnn.py data/en/qa1_single-supporting-fact_train.txt
+    python2 code/main.py GRU 10 data/en/qa1_single-supporting-fact_train.txt
     """
-    sentences = read_data.get_sentences(sys.argv[1])
-    encoding = read_data.sentences_to_word_encodings(sentences)
+    if len(sys.argv) != 4:
 
-    # create the input (X) and desired output (Y)
-    X = [encode_sentence(s, encoding) for s in sentences]
-    Y = [encode_sentence(s, encoding, reversed=True) for s in sentences]
+        print "Usage: main.py GRU 10 [DATA_SET]"
 
-    rnn = Seq2Seq_RNN(K=39)
+    else:
 
-    ref_output = T.matrix()
-    error = T.sum(abs(rnn.decoder.output - ref_output))
-    error_f = theano.function(inputs=[rnn.decoder.output, ref_output],
-                              outputs=error)
+        sentences = read_data.get_sentences(sys.argv[3])
+        encoding  = read_data.sentences_to_word_encodings(sentences)
 
-    gradient = T.grad(error, wrt=rnn.params)
-    train_updates = [(p, p - 0.01 * grad_p)
-                     for (p, grad_p) in zip(rnn.params, gradient)]
+        # create the input (X) and desired output (Y)
+        X = [encode_sentence(s, encoding) for s in sentences]
+        Y = [encode_sentence(s, encoding, reversed=True) for s in sentences]
 
-    train = theano.function(
-        inputs  = [rnn.encoder.input, rnn.decoder.length, ref_output],
-        outputs = error,
-        updates = train_updates)
+        rnn = Seq2Seq_RNN(NNClass=NN_CLASS_DICT[sys.argv[1]], K=39)
 
-    n_epochs = int(sys.argv[2])
-    train_reference_pairs = zip(X, Y)
-    for i in range(n_epochs):
-        print 'Epoch {}:'.format(i)
-        train_idxs = np.random.choice(len(train_reference_pairs), 100)
-        for idx in train_idxs:
-            sentence, ref = train_reference_pairs[idx]
-            print 'Error: {}'.format(train(sentence, len(sentence), ref))
+        ref_output = T.matrix()
+        error = T.sum(abs(rnn.decoder.output - ref_output))
+        error_f = theano.function(
+            inputs=[rnn.decoder.output, ref_output],
+            outputs=error)
 
-    for sentence, ref in train_reference_pairs[:10]:
-        s = rnn.run_final(sentence, len(sentence))
-        print 'Sentence: {}'.format(' '.join(decode_sentence(sentence, encoding)))
-        print 'Reversed: {}'.format(' '.join(decode_sentence(s, encoding)))
-        print ''
+        gradient = T.grad(error, wrt=rnn.params)
+        train_updates = [(p, p - 0.01 * grad_p)
+                         for (p, grad_p) in zip(rnn.params, gradient)]
+
+        train = theano.function(
+            inputs  = [rnn.encoder.input, rnn.decoder.length, ref_output],
+            outputs = error,
+            updates = train_updates)
+
+        n_epochs = int(sys.argv[2])
+        train_reference_pairs = zip(X, Y)
+        for i in range(n_epochs):
+            print 'Epoch {}:'.format(i)
+            train_idxs = np.random.choice(len(train_reference_pairs), 100)
+            for idx in train_idxs:
+                sentence, ref = train_reference_pairs[idx]
+                print 'Error: {}'.format(train(sentence, len(sentence), ref))
+
+        for sentence, ref in train_reference_pairs[:10]:
+            s = rnn.run_final(sentence, len(sentence))
+            print 'Sentence: {}'.format(' '.join(decode_sentence(sentence, encoding)))
+            print 'Reversed: {}'.format(' '.join(decode_sentence(s, encoding)))
+            print ''
 
 if __name__ == '__main__':
     main()
